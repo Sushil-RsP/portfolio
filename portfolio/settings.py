7cccfc26746bf2118b70d5108648b4770b6df394
Sushil-RsP <sushilchavan2468@gamil.com>
2025-07-31 02:29:37 +0530

    Add Render domain to ALLOWED_HOSTS

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -183,13 +183,14 @@ import os
 SECRET_KEY = os.environ.get('SECRET_KEY')
 DEBUG = os.environ.get('DEBUG') == 'True'
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '127.0.0.1,localhost').split(',')
+#ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '127.0.0.1,localhost,192.168.1.5').split(',')
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '192.168.1.10']  # Replace with your laptop IP
 
 EMAIL_HOST = os.environ.get('EMAIL_HOST')
-EMAIL_PORT = int(os.environ.get('EMAIL_PORT'))
+#EMAIL_PORT = int(os.environ.get('EMAIL_PORT'))
+EMAIL_PORT = int(os.environ.get('EMAIL_PORT', 587)) 
 EMAIL_USE_TLS = os.environ.get('EMAIL_USE_TLS') == 'True'
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
-
 
 #DEFAULT_FROM_EMAIL = 'sushilchavan2468@gmail.com'
