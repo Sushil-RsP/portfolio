d9aecb284e4f7c2ff5d534ed64e76e08115ca545
Sushil-RsP <sushilchavan2468@gamil.com>
2025-07-31 02:36:10 +0530

    Add Render domain to ALLOWED_HOSTS

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -183,14 +183,22 @@ import os
 SECRET_KEY = os.environ.get('SECRET_KEY')
 DEBUG = os.environ.get('DEBUG') == 'True'
 
-#ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '127.0.0.1,localhost,192.168.1.5').split(',')
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '192.168.1.10']  # Replace with your laptop IP
-
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '127.0.0.1,localhost,192.168.1.5').split(',')
+#ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '192.168.1.10']  # Replace with your laptop IP
+'''
 EMAIL_HOST = os.environ.get('EMAIL_HOST')
 #EMAIL_PORT = int(os.environ.get('EMAIL_PORT'))
 EMAIL_PORT = int(os.environ.get('EMAIL_PORT', 587)) 
 EMAIL_USE_TLS = os.environ.get('EMAIL_USE_TLS') == 'True'
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD') '''
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = os.environ.get('EMAIL_HOST', 'smtp.gmail.com')
+EMAIL_PORT = int(os.environ.get('EMAIL_PORT', 587))
+EMAIL_USE_TLS = os.environ.get('EMAIL_USE_TLS', 'True') == 'True'
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', 'sushilchavan2468@gmail.com')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', 'crujmbzuyntkqzgk')
+
 
 #DEFAULT_FROM_EMAIL = 'sushilchavan2468@gmail.com'
